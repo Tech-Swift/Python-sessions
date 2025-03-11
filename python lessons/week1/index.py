61f1749553d629d9b55ac4029a7e764e37f531f9
Ticha23 <mutwirigachanja1@gmail.com>
2025-03-11 11:49:01 +0300

    More practice on variables, Data types and combining different data types

--- python lessons/week1/index.py
+++ python lessons/week1/index.py
@@ -1,1 +1,28 @@
 print("Hello world")
+
+# variables
+number = 10
+print(number)
+
+age = 27   # interger datatype
+marks = 89.5  #float datatype
+
+name = "Nelson Muchoki"   #string datatype
+
+#data types
+
+languages = ["Python", "CSS", "PHP", 3.5, 5,7, False]
+
+print(languages)
+print(languages[3]) #this will print the 4 item on the list since counting start from 0
+
+# Combining int, float, and string
+number = 10
+marks = 89.5
+name = "Nelson Muchoki"
+
+# Using f-string
+print(f"Name: {name}, Number: {number}, Marks: {marks}")
+
+# Using str() function
+print("Name: " + name + ", Number: " + str(number) + ", Marks: " + str(marks))
